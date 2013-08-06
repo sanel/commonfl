3669a8fb96a6c7e62959b5c1a10dfaab0dd87d26
Sanel Zukan <sanelz@gmail.com>
2013-08-06 03:50:58 +0200

    Callback example added.

--- test/sample.lisp
+++ test/sample.lisp
@@ -7,11 +7,19 @@
 
 (in-package :fltk-sample)
 
+(defun fl_widget_event (widget func)
+  (cffi:defcallback cb :void ((obj :pointer) (param :pointer))
+	(funcall func))
+  (fl_widget_callback widget (cffi:callback cb) 0))
+
 (defun main ()
   (let ((win    (new_fl_window_3 300 200 ""))
 		(button (new_fl_button 10 10 90 25 "")))
 	(fl_widget_copy_label win "This is label")
 	(fl_widget_copy_label button "Click me")
+	(fl_widget_event button
+					 (lambda ()
+					   (print "zzz")))
 	(fl_group_add win button)
 	(fl_window_show win)
 	(fl_run)))
